bf1754b309e8356d2d74118e8e31873d2f686b5d
Cedric van Putten <me@bycedric.com>
2020-08-07 15:06:57 +0200

    example of bare ota

--- App.tsx
+++ App.tsx
@@ -1,11 +1,43 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useCallback } from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import * as Updates from 'expo-updates';
+
+const states = {
+  idle: null,
+  hasUpdate: 'hasUpdate',
+  downloadUpdate: 'downloadUpdate',
+  updateReady: 'updateReady',
+};
 
 export default function App() {
+  const [state, setState] = useState<string | null>(states.idle);
+
+  const checkForUpdate = useCallback(async () => {
+    const update = await Updates.checkForUpdateAsync();
+    if (update.isAvailable) {
+      setState(states.hasUpdate);
+    }
+  }, []);
+
+  const downloadTheUpdate = useCallback(async () => {
+    setState(states.downloadUpdate);
+    const update = await Updates.fetchUpdateAsync();
+    if (update.isNew) {
+      setState(states.updateReady);
+    }
+  }, []);
+
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Should be visible</Text>
+
+      {state === states.idle && <Button title="Check for updates" onPress={checkForUpdate} />}
+      {state === states.hasUpdate && <Button title="Download the update" onPress={downloadTheUpdate} />}
+      {state === states.downloadUpdate && <Button title="Downloading..." onPress={() => undefined} disabled />}
+      {state === states.updateReady && <Button title="Reload for update" onPress={Updates.reloadAsync} />}
+
       <StatusBar style="auto" />
     </View>
   );
